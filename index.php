7aa4bab495f28ba646384a7d3d9bc2a081860aee
cloudleaders <42769049+cloudleaders@users.noreply.github.com>
2018-08-28 16:35:36 +0530

    error in php file is rectified

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+
+    <head>
+        <title>Hello Cloud Leaders</title>
+    </head>
+
+    <body>
+<div align="center">
+                <h1>Hello Cloud Leader's</h1>
+                <h2>Congratulations! You have successfully installed an application to OpsWorks!</h2>
+                <div align="center"><img src="https://s3-us-west-2.amazonaws.com/cloudleader-php/cloudLead.png"></div>
+                <p>You ACloudLeader Application is now running on the host &ldquo;<?php echo gethostname(); ?>&rdquo; in a new stack and layer in OpsWorks.</p>
+                <p>This host is running PHP version <?php echo phpversion(); ?>.</p>
+        </div>
+    </body>
+
+</html>
